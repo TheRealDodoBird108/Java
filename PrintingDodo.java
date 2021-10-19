6852cb25ce1518d0316803178281c921c5dd6762
TheRealDodoBird108 <89094200+TheRealDodoBird108@users.noreply.github.com>
2021-10-19 18:24:12 +0200

    Add files via upload

---
+++ PrintingDodo.java
@@ -0,0 +1,34 @@
+public class PrintingDodo{
+    //this is how you write a comment!
+    
+    /*
+    This is another way to write a comment!
+    */
+
+    public static void main (String[] args) {
+    
+    System.out.println("Hello World!"); //Print ln allows you to put a space between each printing statment
+    
+    System.out.println("I want to make my program look cool!");
+    
+    System.out.println("Look\n\twhat\n\t\tI\n\t\t\tcan\n\t\t\t\tdo!");
+    
+    /*
+    If you look at [line 14] you can see that I have a bunch of \n's and \t's,
+    This allows you to change the formation of the text. throught the single line!
+    \n forms a new line
+    \t is a tab! (press tab and you'll see what I mean!)
+    */
+
+    System.out.println("I could write extra \"quotation\" marks like this!");
+    
+    /*
+    [line 23] to use quotation marks you write a back slash then a quotation mark!
+    [\" insert text here \"]   
+    */
+
+    }
+    
+}
+
+
