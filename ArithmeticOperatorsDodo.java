b52fae298905e9e643a28c6889a27922be0aea44
TheRealDodoBird108 <89094200+TheRealDodoBird108@users.noreply.github.com>
2021-11-05 22:46:55 +0200

    Add files via upload

---
+++ ArithmeticOperatorsDodo.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperatorsDodo {
+    
+
+    public static void main(String[] args){
+        /*
+        Easier way to declare values, rather than declaring int every time.
+        */
+        int FirstNum = 100, SecondNum = 290, ThirdNum = 150;
+        
+        System.out.println("First number is equal to " + FirstNum);
+        System.out.println("Second number " +"is equal to " + SecondNum);
+        System.out.println("Third number " + 
+                                        "is equal to " + SecondNum); //Format doesnt matter as much!
+        // adding num1 and num2
+        final int TheSumOfNumbers = FirstNum + SecondNum + ThirdNum; //(final) doesnt allow the varible to change no matter what
+        System.out.println("The sum = " + TheSumOfNumbers);
+
+
+    }
+
+}
